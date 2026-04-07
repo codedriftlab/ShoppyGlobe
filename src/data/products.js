20f7bf618d3a45a1735f0ee6610809e3f317ddf7
codedriftlab <tempmail3470@gmail.com>
2026-04-07 16:17:52 +0530

    image update

--- src/data/products.js
+++ src/data/products.js
@@ -11,7 +11,7 @@ const products = [
     title: "Samsung Galaxy S22",
     price: 70000,
     description: "Samsung flagship phone",
-    thumbnail: "https://m.media-amazon.com/images/I/71fVoqRC0wL._SX679_.jpg"
+    thumbnail: "https://api.samsungmobilepress.com/api/v1/file/BEB4E9A0290E5C11DCB88E288202ECCC2AF1A742231242CF82FC6A93EDC5CFAB8CFA4E6088D1032E93F9FAC5885765772B6FEAA92302243DCCB425FE739516954B13249ADA5794A4C0B64E7EE2AF35E278137C136B5149F3759008768EEEE73484EDA77B2F0E2B093A04CF55A8C92826C53F1D49304971AD8C2D29F41FDC0F1E428A66843D54CD7D6DE09C044B169CBB"
   },
   {
     id: 3,
@@ -25,14 +25,14 @@ const products = [
     title: "HP Laptop",
     price: 55000,
     description: "HP powerful laptop",
-    thumbnail: "https://m.media-amazon.com/images/I/71TPda7cwUL._SX679_.jpg"
+    thumbnail: "https://hp.widen.net/content/okaik5jt1f/webp/okaik5jt1f.png?w=320&h=240&dpi=72&color=ffffff00"
   },
   {
     id: 5,
     title: "Dell Inspiron",
     price: 65000,
     description: "Dell reliable laptop",
-    thumbnail: "https://m.media-amazon.com/images/I/61uZbP9SThL._SX679_.jpg"
+    thumbnail: "https://dellstatic.luroconnect.com/media/catalog/product/cache/74ae05ef3745aec30d7f5a287debd7f5/i/n/inspiron-14-7440-2-in-1-laptop-pn5122w-in-festive3.png"
   },
   {
     id: 6,
@@ -53,14 +53,14 @@ const products = [
     title: "Sony Headphones",
     price: 5000,
     description: "Noise cancelling headphones",
-    thumbnail: "https://m.media-amazon.com/images/I/61v5xZRE2gL._SX679_.jpg"
+    thumbnail: "https://sony.scene7.com/is/image/sonyglobalsolutions/WH-CH720N_Product_intro_Pink_01_M?$productIntroPlatemobile$&fmt=png-alpha"
   },
   {
     id: 9,
     title: "JBL Speaker",
     price: 4000,
     description: "Portable speaker",
-    thumbnail: "https://m.media-amazon.com/images/I/81JZ7L3d5IL._SX679_.jpg"
+    thumbnail: "https://m.media-amazon.com/images/I/81MjIZM2H9L._SX679_.jpg"
   },
   {
     id: 10,
@@ -74,42 +74,42 @@ const products = [
     title: "Mi Band",
     price: 2000,
     description: "Fitness band",
-    thumbnail: "https://m.media-amazon.com/images/I/61mpMH5TzkL._SX679_.jpg"
+    thumbnail: "https://www.desertcart.in/_next/image?url=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F51bkPDf8dUL.jpg&w=128&q=75"
   },
   {
     id: 12,
     title: "Gaming Mouse",
     price: 1500,
     description: "RGB gaming mouse",
-    thumbnail: "https://m.media-amazon.com/images/I/61LtuGzXeaL._SX679_.jpg"
+    thumbnail: "https://m.media-amazon.com/images/I/61AcT0ZuO3L._SX679_.jpg"
   },
   {
     id: 13,
     title: "Mechanical Keyboard",
     price: 4500,
     description: "RGB keyboard",
-    thumbnail: "https://m.media-amazon.com/images/I/71Kr3WAj1FL._SX679_.jpg"
+    thumbnail: "https://vrkaa.com/cdn/shop/files/61XqYeYoEZL._UF1000_1000_QL80.jpg?v=1754300464&width=990"
   },
   {
     id: 14,
     title: "24 inch Monitor",
     price: 12000,
     description: "Full HD monitor",
-    thumbnail: "https://m.media-amazon.com/images/I/71h2c5oH9JL._SX679_.jpg"
+    thumbnail: "https://image.benq.com/is/image/benqco/pv3200u-1?$ResponsivePreset$&fmt=png-alpha"
   },
   {
     id: 15,
     title: "Power Bank",
     price: 1200,
     description: "Fast charging power bank",
-    thumbnail: "https://m.media-amazon.com/images/I/61D+2r2s4zL._SX679_.jpg"
+    thumbnail: "https://m.media-amazon.com/images/I/61NONhp2YZL._SX679_.jpg"
   },
   {
     id: 16,
     title: "Bluetooth Earbuds",
     price: 2500,
     description: "Wireless earbuds",
-    thumbnail: "https://m.media-amazon.com/images/I/61CGHv6kmWL._SX679_.jpg"
+    thumbnail: "https://m.media-amazon.com/images/I/61QyJsjiBcL._SX679_.jpg"
   },
 
   // ✅ New 4 items
